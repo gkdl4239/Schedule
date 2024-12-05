e1c7dbca980b93c094f85a6d09672f410bda8d72
gkdl4239 <gkdl4239@gmail.com>
2024-12-05 11:51:39 +0900

    feat : 작성자 테이블 필드 및 생성자 추가

--- src/main/java/com/example/schedule/entity/Author.java
+++ src/main/java/com/example/schedule/entity/Author.java
@@ -1,4 +1,22 @@
 package com.example.schedule.entity;
 
+import lombok.Getter;
+
+@Getter
 public class Author {
+
+    private Long id;
+    private String name;
+    private String email;
+
+    public Author(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+
+    public Author(Long id, String name, String email){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 }
