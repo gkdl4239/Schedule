70bcd93484291d87c67edb5454f40c0e4b1fc36d
gkdl4239 <gkdl4239@gmail.com>
2024-12-05 11:56:31 +0900

    fix : 테이블 구조 변경에 따른 대대적인 생성, 조회 API 로직 변경

--- src/main/java/com/example/schedule/repository/ScheduleRepositoryImpl.java
+++ src/main/java/com/example/schedule/repository/ScheduleRepositoryImpl.java
@@ -1,7 +1,9 @@
 package com.example.schedule.repository;
 
 import com.example.schedule.dto.ResponseDto;
+import com.example.schedule.entity.Author;
 import com.example.schedule.entity.Schedule;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -19,86 +21,110 @@ import java.util.List;
 import java.util.Map;
 
 @Repository
-public class ScheduleRepositoryImpl implements  ScheduleRepository{
+public class ScheduleRepositoryImpl implements  ScheduleRepository {
 
     private final JdbcTemplate jdbcTemplate;
 
-    public ScheduleRepositoryImpl(DataSource dataSource){
+    public ScheduleRepositoryImpl(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     @Override
-    public ResponseDto saveSchedule(Schedule schedule) {
+    public ResponseDto saveSchedule(Schedule schedule, Author author) {
 
-        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
-        jdbcInsert.withTableName("schedule").usingGeneratedKeyColumns("id");
+        String checkAuthor = "SELECT id FROM author WHERE name = ? AND email = ?";
+        Long authorId;
+        try{
+            authorId = jdbcTemplate.queryForObject(checkAuthor,Long.class,author.getName(),author.getEmail());
+        }
+
+        catch(EmptyResultDataAccessException e){
+            SimpleJdbcInsert authorInsert = new SimpleJdbcInsert(jdbcTemplate)
+                    .withTableName("author")
+                    .usingGeneratedKeyColumns("id")
+                    .usingColumns("name","email");
+
+            Map<String, Object> authorParams = new HashMap<>();
+            authorParams.put("name",author.getName());
+            authorParams.put("email",author.getEmail());
+
+            Number generatedId = authorInsert.executeAndReturnKey(new MapSqlParameterSource(authorParams));
+            authorId = generatedId.longValue();
+        }
+
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("schedule")
+                .usingGeneratedKeyColumns("id")
+                .usingColumns("toDo","password","author_id");
 
         Map<String, Object> parameters = new HashMap<>();
-        LocalDateTime now = LocalDateTime.now();
-        parameters.put("name",schedule.getName());
-        parameters.put("toDo",schedule.getToDo());
-        parameters.put("password",schedule.getPassword());
-        parameters.put("createdDate",now);
-        parameters.put("modifiedDate",now);
+        parameters.put("toDo", schedule.getToDo());
+        parameters.put("password", schedule.getPassword());
+        parameters.put("author_id", authorId);
 
         Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
 
-        return new ResponseDto(key.longValue(), schedule.getToDo(), schedule.getName(), now);
+        return new ResponseDto(key.longValue(), schedule.getToDo(), author.getName(), LocalDateTime.now());
     }
 
     @Override
-    public List<ResponseDto> findAllSchedule(String name,String period, LocalDateTime startDate,LocalDateTime endDate) {
+    public List<ResponseDto> findAllScheduleByAuthorId(String name, String period, LocalDateTime startDate, LocalDateTime endDate) {
 
-        if(!"custom".equals(period))
-        {
+        if (!"custom".equals(period)) {
             LocalDateTime now = LocalDateTime.now();
             endDate = now;
-            switch(period){
+            switch (period) {
                 case "1hour" -> startDate = now.minusHours(1);
                 case "1day" -> startDate = now.minusDays(1);
                 case "1week" -> startDate = now.minusWeeks(1);
                 case "1month" -> startDate = now.minusMonths(1);
                 case "3months" -> startDate = now.minusMonths(3);
                 case "6months" -> startDate = now.minusMonths(6);
-                case "1year" ->startDate =  now.minusYears(1);
+                case "1year" -> startDate = now.minusYears(1);
                 default -> throw new IllegalArgumentException("Invalid period: " + period);
             }
-        }
-        else{
+        } else {
             endDate = endDate.plusDays(1);
         }
-        return jdbcTemplate.query("SELECT * FROM schedule WHERE name = ? AND modifiedDate BETWEEN ? AND ?",scheduleRowMapper(),name,startDate,endDate);
+        String sql = "SELECT s.id, s.toDo, s.modifiedDate, a.name, a.email " +
+                "FROM schedule AS s JOIN author AS a " +
+                "ON s.author_id = a.id " +
+                "WHERE a.name = ? AND a.email = ? AND s.modifiedDate BETWEEN ? AND ?";
+        return jdbcTemplate.query(sql, scheduleRowMapper(), name,email, startDate, endDate);
 
     }
 
     @Override
-    public Schedule findScheduleById(Long id) {
+    public ResponseDto findScheduleById(Long id) {
 
-        List<Schedule> result = jdbcTemplate.query("SELECT * FROM schedule WHERE id = ?",scheduleRowMapperV2(),id);
-        return result.stream().findAny().orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        String sql = "SELECT * FROM schedule AS s JOIN author as a ON s.author_id = a.id WHERE s.id = ?";
+        List<ResponseDto> result = jdbcTemplate.query(sql, scheduleRowMapper(), id);
+        return result
+                .stream()
+                .findAny()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     @Override
     public int updateToDoAndName(Long id, String name, String toDo, String password) {
         LocalDateTime now = LocalDateTime.now();
-        if(password.equals(jdbcTemplate.queryForObject("SELECT password FROM schedule WHERE id = ?",new Object[]{id}, String.class))){
-            return jdbcTemplate.update("UPDATE schedule SET name = ?, toDo = ?, modifiedDate = ? WHERE id = ?",name,toDo,id,now);
-        }
-        else{
+        if (password.equals(jdbcTemplate.queryForObject("SELECT password FROM schedule WHERE id = ?", new Object[]{id}, String.class))) {
+            return jdbcTemplate.update("UPDATE schedule SET name = ?, toDo = ?, modifiedDate = ? WHERE id = ?", name, toDo, id, now);
+        } else {
             return 0;
         }
     }
 
     @Override
     public int deleteSchedule(Long id, String password) {
-        if(password.equals(jdbcTemplate.queryForObject("SELECT password FROM schedule WHERE id = ?",new Object[]{id}, String.class))){
-            return jdbcTemplate.update("DELETE FROM schedule WHERE id = ? AND password = ?",id, password);
+        if (password.equals(jdbcTemplate.queryForObject("SELECT password FROM schedule WHERE id = ?", new Object[]{id}, String.class))) {
+            return jdbcTemplate.update("DELETE FROM schedule WHERE id = ? AND password = ?", id, password);
         }
         return 0;
     }
 
 
-    private RowMapper<ResponseDto> scheduleRowMapper()  {
+    private RowMapper<ResponseDto> scheduleRowMapper() {
 
         return new RowMapper<>() {
             @Override
@@ -107,21 +133,6 @@ public class ScheduleRepositoryImpl implements  ScheduleRepository{
                         rs.getLong("id"),
                         rs.getString("toDo"),
                         rs.getString("name"),
-                        rs.getTimestamp("modifiedDate").toLocalDateTime()
-                );
-            }
-        };
-    }
-
-    private RowMapper<Schedule> scheduleRowMapperV2() {
-
-        return new RowMapper<>() {
-            @Override
-            public Schedule mapRow(ResultSet rs, int rowNum) throws SQLException {
-                return new Schedule(
-                        rs.getLong("id"),
-                        rs.getString("name"),
-                        rs.getString("toDo"),
                         rs.getTimestamp("modifiedDate").toLocalDateTime()
                 );
             }
