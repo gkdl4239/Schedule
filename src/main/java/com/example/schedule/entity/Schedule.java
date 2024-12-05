ffd75c88e9ead4b69451fffe61ce52b4e50d98c9
gkdl4239 <gkdl4239@gmail.com>
2024-12-05 11:54:29 +0900

    fix : 테이블 구조 변경에 따른 개체 구조 변경

--- src/main/java/com/example/schedule/entity/Schedule.java
+++ src/main/java/com/example/schedule/entity/Schedule.java
@@ -9,22 +9,18 @@ public class Schedule {
 
     private Long id;
     private String toDo;
-    private String name;
     private String password;
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
 
-    public Schedule(String name, String toDo, String password) {
-        this.name = name;
+    public Schedule(String toDo, String password) {
         this.toDo = toDo;
         this.password = password;
     }
 
-    public Schedule(Long id,String name, String toDo, LocalDateTime modifiedDate) {
+    public Schedule(Long id, String toDo, LocalDateTime modifiedDate) {
         this.id = id;
-        this.name = name;
         this.toDo = toDo;
-
         this.modifiedDate = modifiedDate;
     }
 }
