fd9b3914ec355341377958d158a81a6cea1622cd
gkdl4239 <gkdl4239@gmail.com>
2024-12-06 23:56:33 +0900

    refactor : 사용하지 않는 구문 삭제 및 주석 처리

--- src/main/java/com/example/schedule/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/schedule/exception/GlobalExceptionHandler.java
@@ -5,21 +5,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.View;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
+
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    private final View error;
-
-    public GlobalExceptionHandler(View error) {
-        this.error = error;
-    }
-
+    // 사용자 정의 예외
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponseDto> handleCustomException(CustomException e){
         ErrorResponseDto errorResponse = new ErrorResponseDto(
@@ -29,6 +22,7 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorResponse, e.getStatus());
     }
 
+    // 나머지 일반적인 예외
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto> handleException(Exception e){
         ErrorResponseDto errorResponse = new ErrorResponseDto(
