d1654548c35312aa706ea4e71942d2c5f423c025
gkdl4239 <gkdl4239@gmail.com>
2024-12-06 20:49:29 +0900

    fix : Restful Api를 위해 GET API 호출시 RequestBody 가 아닌 RequestParams 로 요청값 전달

--- src/main/java/com/example/schedule/controller/ScheduleController.java
+++ src/main/java/com/example/schedule/controller/ScheduleController.java
@@ -4,10 +4,13 @@ import com.example.schedule.dto.PageResponseDto;
 import com.example.schedule.dto.ScheduleRequestDto;
 import com.example.schedule.dto.ScheduleResponseDto;
 import com.example.schedule.service.ScheduleService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
@@ -28,8 +31,19 @@ public class ScheduleController {
     }
 
     @GetMapping
-    public ResponseEntity<PageResponseDto<ScheduleResponseDto>> findAllSchedule(@RequestBody ScheduleRequestDto dto){
-        PageResponseDto<ScheduleResponseDto> result = scheduleService.findAllSchedule(dto.getName(),dto.getEmail(),dto.getPeriod(),dto.getStartDate(),dto.getEndDate(),dto.getSize(),dto.getPage());
+    public ResponseEntity<PageResponseDto<ScheduleResponseDto>> findAllSchedule(
+            @RequestParam(required = false) Long id,
+            @RequestParam(required = false) String period,
+            @RequestParam(required = false) String startDate,
+            @RequestParam(required = false) String endDate,
+            @RequestParam(defaultValue = "5") int size,
+            @RequestParam(defaultValue = "1") int page
+    ){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime start = LocalDateTime.parse(startDate, formatter);
+        LocalDateTime end = LocalDateTime.parse(endDate, formatter);
+
+        PageResponseDto<ScheduleResponseDto> result = scheduleService.findAllSchedule(id, period, start, end, size, page);
         return ResponseEntity.ok(result);
     }
 
