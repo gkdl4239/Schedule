4b92f83ba109b92fb0251881860716277fb048f1
gkdl4239 <gkdl4239@gmail.com>
2024-12-06 21:09:23 +0900

    feat : Valid 어노테이션을 통한 유효하게 값 처리

--- src/main/java/com/example/schedule/dto/ScheduleRequestDto.java
+++ src/main/java/com/example/schedule/dto/ScheduleRequestDto.java
@@ -2,17 +2,27 @@ package com.example.schedule.dto;
 
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import jakarta.validation.constraints.Email;
 
 import java.time.LocalDateTime;
 
 @Getter
 public class ScheduleRequestDto {
+
+    @Max(value = 200, message = "최대 200자까지 입력 가능합니다.")
     private String toDo;
+
     private String name;
+
+    @Email(message = "유효한 이메일 형식이어야 합니다")
     private String email;
+
+    @NotNull
     private String password;
+
     private String period;
     private int page = 1;
     private int size = 5;
