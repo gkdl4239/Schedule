e6660c4cbf5aa39911ceb285828747a34ae65499
gkdl4239 <gkdl4239@gmail.com>
2024-12-08 16:14:08 +0900

    fix : 비밀번호 확인 후 무조건 예외 던지는 오류 수정 및 주석 처리

--- src/main/java/com/example/schedule/repository/ScheduleRepositoryImpl.java
+++ src/main/java/com/example/schedule/repository/ScheduleRepositoryImpl.java
@@ -35,8 +35,11 @@ public class ScheduleRepositoryImpl implements ScheduleRepository {
     public ScheduleResponseDto saveSchedule(String name, String email, String toDo, String password) {
 
 
+        // 요청값으로 받은 이름과 이메일로 기존 작성자 정보 존재 유무 확인
         String checkAuthor = "SELECT id FROM author WHERE name = ? AND email = ?";
         Long authorId;
+
+        // 작성자 id가 존재하면 id 값 받아옴 OR 존재하지 않으면 작성자 테이블에 레코드 생성
         try {
             authorId = jdbcTemplate.queryForObject(checkAuthor, Long.class, name, email);
         } catch (EmptyResultDataAccessException e) {
@@ -53,6 +56,7 @@ public class ScheduleRepositoryImpl implements ScheduleRepository {
             authorId = generatedId.longValue();
         }
 
+        // 구한 작성자 ID를 바탕으로 나머지 속성과 함께 일정 레코드도 테이블에 생성
         SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
                 .withTableName("schedule")
                 .usingGeneratedKeyColumns("id")
@@ -71,7 +75,7 @@ public class ScheduleRepositoryImpl implements ScheduleRepository {
     @Override
     public PageResponseDto<ScheduleResponseDto> findAllScheduleByAuthorId(Long id, String period, LocalDateTime startDate, LocalDateTime endDate, int size, int page) {
 
-
+        // 작성자 및 일정 테이블을 조인하여 검색 ( 식별자, 수정일 통합 )
         StringBuilder sql = new StringBuilder("""
                 SELECT s.id, s.toDo, s.modifiedDate, a.name, a.email
                 FROM schedule AS s
@@ -82,18 +86,21 @@ public class ScheduleRepositoryImpl implements ScheduleRepository {
 
         buildWhereClause(sql, params, id, startDate, endDate);
 
+        // 페이징을 위한 검색된 레코드 총 개수 연산
         StringBuilder countSql = new StringBuilder("""
                 SELECT COUNT(*)
                 FROM schedule AS s
                 JOIN author AS a ON s.author_id = a.id
                 """);
 
+        // 정확한 총 개수 연산을 위한 앞서 실행한 쿼리문의 조건 복사
         if (sql.indexOf("WHERE") != -1) {
             countSql.append(sql.substring(sql.indexOf("WHERE")));
         }
 
         int totalElements = jdbcTemplate.queryForObject(countSql.toString(), Integer.class, params.toArray());
 
+        // 페이지 당 사이즈와 현재 페이지 위치 구하기
         sql.append("ORDER BY s.modifiedDate DESC LIMIT ? OFFSET ?");
         params.add(size);
         params.add((page - 1) * size);
@@ -101,9 +108,9 @@ public class ScheduleRepositoryImpl implements ScheduleRepository {
 
         List<ScheduleResponseDto> schedules = jdbcTemplate.query(sql.toString(), scheduleRowMapper(), params.toArray());
 
-
         int totalPages = (int) Math.ceil((double) totalElements / size);
 
+        // 현재 페이지는 마지막 페이지 초과 불가
         if (page > totalPages) {
             page = Math.max(totalPages, 1);
         }
@@ -111,16 +118,22 @@ public class ScheduleRepositoryImpl implements ScheduleRepository {
         return new PageResponseDto<>(schedules, page, size, totalPages, totalElements);
     }
 
+    // 조건에 따른 sql 쿼리문 추가
     private void buildWhereClause(StringBuilder sql, List<Object> params, Long id,
                                   LocalDateTime startDate, LocalDateTime endDate) {
+
+        // 필터가 1개라도 있어야 WHERE 문 시작
         if (id != null || endDate != null) {
             sql.append("WHERE ");
         }
+
+        // 식별자 필터링
         if (id != null) {
             sql.append("s.author_id = ? ");
             params.add(id);
         }
 
+        // 수정일 필터링
         if (startDate != null && endDate != null) {
             if (id != null) {
                 sql.append("AND ");
@@ -149,6 +162,7 @@ public class ScheduleRepositoryImpl implements ScheduleRepository {
 
         isValidInTable(id);
 
+        // 비밀번호가 같으면 이름,할일 둘다 변경하거나 하나만 변경 가능
         if (password.equals(searchPassword(id))) {
 
             if (name != null) {
@@ -159,8 +173,9 @@ public class ScheduleRepositoryImpl implements ScheduleRepository {
             if (toDo != null) {
                 jdbcTemplate.update("UPDATE schedule SET toDo = ? WHERE id = ?", toDo, id);
             }
+        } else {
+            throw new BadRequestException("비밀번호가 올바르지 않습니다 !");
         }
-        throw new BadRequestException("비밀번호가 올바르지 않습니다 !");
     }
 
     @Override
@@ -170,10 +185,11 @@ public class ScheduleRepositoryImpl implements ScheduleRepository {
 
         if (password.equals(searchPassword(id))) {
             jdbcTemplate.update("DELETE FROM schedule WHERE id = ? AND password = ?", id, password);
+        } else {
+            throw new BadRequestException("비밀번호가 올바르지 않습니다 !");
         }
-
-        throw new BadRequestException("비밀번호가 올바르지 않습니다 !");
     }
+
 
     private void isValidInTable(Long id) {
 
