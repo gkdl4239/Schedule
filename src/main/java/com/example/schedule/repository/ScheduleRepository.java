58d671c9642f0fb4be9aa56b2b53c1a6c7803f1a
gkdl4239 <gkdl4239@gmail.com>
2024-12-08 14:51:55 +0900

    refactor : 개체가 아닌 dto에서 다이렉트로 입력값 삽입으로 변경

--- src/main/java/com/example/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/example/schedule/repository/ScheduleRepository.java
@@ -2,15 +2,14 @@ package com.example.schedule.repository;
 
 import com.example.schedule.dto.PageResponseDto;
 import com.example.schedule.dto.ScheduleResponseDto;
-import com.example.schedule.entity.Author;
-import com.example.schedule.entity.Schedule;
+
 
 
 import java.time.LocalDateTime;
 
 public interface ScheduleRepository {
 
-    ScheduleResponseDto saveSchedule(Schedule schedule, Author author);
+    ScheduleResponseDto saveSchedule(String name, String email, String toDo, String password);
 
     PageResponseDto<ScheduleResponseDto> findAllScheduleByAuthorId(Long id, String period, LocalDateTime startDate, LocalDateTime endDate, int size, int page);
 
