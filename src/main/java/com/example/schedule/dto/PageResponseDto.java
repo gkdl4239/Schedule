89340b2271af70b8d51c9b22e9732682c349cb8d
gkdl4239 <gkdl4239@gmail.com>
2024-12-08 17:50:02 +0900

    fix : 제네릭 변경 및 생성자 -> AllArgsConstructor 로 대체

--- src/main/java/com/example/schedule/dto/PageResponseDto.java
+++ src/main/java/com/example/schedule/dto/PageResponseDto.java
@@ -1,23 +1,16 @@
 package com.example.schedule.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.List;
 
+@AllArgsConstructor
 @Getter
-public class PageResponseDto<T> {
-    private final List<T> data;
+public class PageResponseDto {
+    private final List<ScheduleResponseDto> data;
     private final int currentPage;
     private final int pageSize;
     private final int totalPages;
     private final long totalElements;
-
-    public PageResponseDto(List<T> data, int currentPage, int pageSize, int totalPages, long totalElements) {
-        this.data = data;
-        this.currentPage = currentPage;
-        this.pageSize = pageSize;
-        this.totalPages = totalPages;
-        this.totalElements = totalElements;
-    }
 }
