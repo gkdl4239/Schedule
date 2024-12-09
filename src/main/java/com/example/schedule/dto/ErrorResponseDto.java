7bb84b8f26188fab664534ac8ee8f973df4896aa
gkdl4239 <gkdl4239@gmail.com>
2024-12-09 10:05:16 +0900

    refactor : 생성자 어노테이션명시로 변경

--- src/main/java/com/example/schedule/dto/ErrorResponseDto.java
+++ src/main/java/com/example/schedule/dto/ErrorResponseDto.java
@@ -1,18 +1,15 @@
 package com.example.schedule.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
+
 @Getter
+@AllArgsConstructor
 public class ErrorResponseDto {
     private final String message;
     private final int status;
     private final LocalDateTime timestamp;
-
-    public ErrorResponseDto(String message, int status, LocalDateTime timestamp) {
-        this.message = message;
-        this.status = status;
-        this.timestamp = timestamp;
-    }
 }
