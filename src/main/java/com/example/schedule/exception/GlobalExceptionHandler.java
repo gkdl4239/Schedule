7cef6d383363d52d88670c2a1e2abf68f962f73d
gkdl4239 <gkdl4239@gmail.com>
2024-12-09 09:53:54 +0900

    feat : Validation 에 대한 예외처리핸들러 추가

--- src/main/java/com/example/schedule/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/schedule/exception/GlobalExceptionHandler.java
@@ -3,10 +3,12 @@ package com.example.schedule.exception;
 import com.example.schedule.dto.ErrorResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+
 
 
 @RestControllerAdvice
@@ -21,6 +23,20 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(errorResponse, e.getStatus());
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDto> handleValidationException(MethodArgumentNotValidException e) {
+
+        String message = e.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
+
+        ErrorResponseDto errorResponse = new ErrorResponseDto(
+                message, e.getStatusCode().value(), LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(errorResponse, e.getStatusCode());
+    }
+
+
 
     // 나머지 일반적인 예외
     @ExceptionHandler(Exception.class)
